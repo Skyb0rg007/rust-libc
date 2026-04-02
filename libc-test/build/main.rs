54ae59ed98fe7ddfe26d170b1fbd17b4cc4241f9
Skye Soss <skye@soss.website>
2026-04-02 16:13:41 -0500

    unix: add types and macros for ICMPv6 filtering
    This commit adds the icmp6_filter type and the corresponding
    ICMP6_FILTER macros. These functions are tested in a similar manner to
    the CMSG macros.

--- libc-test/build/main.rs
+++ libc-test/build/main.rs
@@ -53,6 +53,22 @@ fn do_cc(t: &Target) {
     {
         cc::Build::new().file("src/sigrt.c").compile("sigrt");
     }
+    if (t.linux() && !t.wasm32())
+        || t.android()
+        || t.apple()
+        || t.dragonfly()
+        || t.emscripten()
+        || t.freebsd()
+        || t.fuchsia()
+        || t.illumos()
+        || t.netbsd()
+        || t.openbsd()
+        || t.solaris()
+    {
+        cc::Build::new()
+            .file("src/icmp6_filter.c")
+            .compile("icmp6_filter");
+    }
 }
 
 fn do_ctest(t: &Target) {
@@ -194,6 +210,7 @@ fn test_apple(t: &Target) {
         "netinet/ip.h",
         "netinet/tcp.h",
         "netinet/udp.h",
+        "netinet/icmp6.h",
         "netinet6/scope6_var.h",
         (macos, "netinet6/in6_var.h"),
         "os/clock.h",
@@ -422,6 +439,7 @@ fn test_openbsd(t: &Target) {
         "netinet/ip.h",
         "netinet/tcp.h",
         "netinet/udp.h",
+        "netinet/icmp6.h",
         "net/bpf.h",
         "regex.h",
         "resolv.h",
@@ -967,6 +985,7 @@ fn test_solarish(t: &Target) {
         "netinet/ip.h",
         "netinet/tcp.h",
         "netinet/udp.h",
+        "netinet/icmp6.h",
         "poll.h",
         "port.h",
         "pthread.h",
@@ -1260,6 +1279,7 @@ fn test_netbsd(t: &Target) {
         "netinet/ip.h",
         "netinet/tcp.h",
         "netinet/udp.h",
+        "netinet/icmp6.h",
         "poll.h",
         "pthread.h",
         "pwd.h",
@@ -1561,6 +1581,7 @@ fn test_dragonflybsd(t: &Target) {
         "netinet/ip.h",
         "netinet/tcp.h",
         "netinet/udp.h",
+        "netinet/icmp6.h",
         "paths.h",
         "poll.h",
         "pthread.h",
@@ -2028,6 +2049,7 @@ fn test_android(t: &Target) {
         "netinet/ip.h",
         "netinet/tcp.h",
         "netinet/udp.h",
+        "netinet/icmp6.h",
         "netpacket/packet.h",
         "poll.h",
         "pthread.h",
@@ -2579,6 +2601,7 @@ fn test_freebsd(t: &Target) {
         "netinet/sctp.h",
         "netinet/tcp.h",
         "netinet/udp.h",
+        "netinet/icmp6.h",
         "netinet6/in6_var.h",
         "paths.h",
         "poll.h",
@@ -3216,6 +3239,7 @@ fn test_emscripten(t: &Target) {
         "netinet/ip.h",
         "netinet/tcp.h",
         "netinet/udp.h",
+        "netinet/icmp6.h",
         "netpacket/packet.h",
         "poll.h",
         "pthread.h",
@@ -3496,6 +3520,7 @@ fn test_neutrino(t: &Target) {
         "netinet/ip.h",
         "netinet/tcp.h",
         "netinet/udp.h",
+        "netinet/icmp6.h",
         "netinet/ip_var.h",
         "sys/poll.h",
         "pthread.h",
@@ -3802,6 +3827,7 @@ fn test_vxworks(t: &Target) {
         "sys/mman.h",
         "netinet/tcp.h",
         "netinet/udp.h",
+        "netinet/icmp6.h",
         "netinet/in.h",
         "netinet6/in6.h",
         "syslog.h",
@@ -4045,6 +4071,7 @@ fn test_linux(t: &Target) {
         "netinet/tcp.h",
         "netinet/udp.h",
         (gnu, "netiucv/iucv.h"),
+        "netinet/icmp6.h",
         (l4re, "netpacket/packet.h"),
         "poll.h",
         "pthread.h",
